12f3c1fde933df3292600ac6025093b5e8e1c5a1
Yuito Murase <yuito.murase@gmail.com>
2017-11-05 15:05:18 +0900

    implement input preprocessor

---
+++ tools/makeinput_scm_sr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void
+to_binary(char c, char* buff) {
+  int i = 0;
+  while (c > 0){
+    buff[i * 2] = (c & 1)?'1':'0';
+    buff[i * 2 + 1] = ' ';
+    c >>= 1;
+    i++;
+  }
+  buff[i * 2] = '\0';
+}
+
+int
+main() {
+  printf("(define-syntax input!\n");
+  printf("  (syntax-rules (quote)\n");
+  printf("    ((_ s) (ck s '(\n");
+
+  char c;
+  while ((c = getchar()) != EOF) {
+    char buff[100];
+    to_binary(c, buff);
+    printf("                   (%s)\n", buff);
+  }
+  
+  printf("                  )))))\n");
+}
