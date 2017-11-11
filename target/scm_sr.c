f2d02e58e41591c5b7f6c9b54d984af5383945c6
Yuito Murase <yuito.murase@gmail.com>
2017-11-11 19:34:57 +0900

    fix scm_sr.c (now it can be compiled)

--- target/scm_sr.c
+++ target/scm_sr.c
@@ -1,6 +1,6 @@
 #include <ir/ir.h>
 #include <target/util.h>
-#include "scm_sr_lib.h"
+#include <target/scm_sr_lib.h>
 
 static void scm_sr_emit_file_prologue(void) {
   emit_line(scm_sr_lib);
@@ -14,11 +14,26 @@ static void scm_sr_emit_func_epilogue() {
   /* fixme */
 }
 
+static void scm_sr_emit_func_impl(Inst* inst) {
+  (void) inst;
+  scm_sr_emit_func_prologue();
+  scm_sr_emit_func_epilogue();
+  /* fixme */
+}
+
+static int scm_sc_count_pc(Inst* inst) {
+  (void) inst;
+  return 0;
+  /* fixme */
+}
+
 static void scm_sr_emit_inst_mem(int max_pc) {
+  (void)max_pc;
   /* fixme */
 }
 
 static void scm_sr_emit_data_mem(Data* data) {
+  (void)data;
   /* fixme */
 }
 
@@ -26,7 +41,7 @@ void target_scm_sr(Module* module) {
   scm_sr_emit_file_prologue();
   emit_line("");
 
-  scm_sr_emit_func_switch_impl(module->text);
+  scm_sr_emit_func_impl(module->text);
   emit_line("");
 
   scm_sr_emit_inst_mem(scm_sc_count_pc(module->text));
