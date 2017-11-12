a443cc94217c9bd9b99329ad5b560aa872631011
Yuito Murase <yuito.murase@gmail.com>
2017-11-12 11:55:55 +0900

    implementing emitter for scheme syntax-rules backend

--- target/scm_sr.c
+++ target/scm_sr.c
@@ -6,6 +6,15 @@ static void scm_sr_emit_file_prologue(void) {
   emit_line(scm_sr_lib);
 }
 
+static void scm_sr_emit_file_epilogue(void) {
+  emit_line(";;; Load Input");
+  emit_line("(load \"input.scm\")");
+  emit_line(";;; Run VM!");
+  emit_line("(ck () (run-vm! '\"load\" '() '(() () () () () ())");
+  emit_line("                (dmem-init!) (imem-init!)");
+  emit_line("                (input!) '()))");
+}
+
 static void scm_sr_emit_func_prologue(int i) {
   emit_line("(define-syntax func-impl%d!", i);
   inc_indent();
@@ -59,8 +68,9 @@ static char* scm_sr_op_str(Inst* inst) {
 
 static char* scm_sr_imm_str(int imm) {
   if (imm < 0) {
+    error("invalid immediate");
     return "POHE";
-  } /* TO BE FIXED */
+  }
 
   if (imm == 0) {
     return "";
@@ -166,14 +176,67 @@ static int scm_sc_count_pc(Inst* inst) {
   return max_pc;
 }
 
+static void scm_sr_emit_inst_mem_rec(int max_pc, int from, int to) {
+  if (max_pc < from) {
+    emit_line("'()");
+  } else if (from + 1 >= to) {
+    emit_line("(func_impl%d!)", from);
+  } else {
+    int mid = (from + to)/2;
+    emit_line("(cons!");
+    inc_indent();
+    scm_sr_emit_inst_mem_rec(max_pc, from, mid);
+    scm_sr_emit_inst_mem_rec(max_pc, mid, to);
+    dec_indent();
+    emit_line(")");
+  }
+}
+
 static void scm_sr_emit_inst_mem(int max_pc) {
-  (void)max_pc;
-  /* fixme */
+  emit_line("(define-syntax imem-init!");
+  inc_indent();
+  emit_line("(syntax-rules (quote) ((_ s) (ck s");
+  inc_indent();
+  scm_sr_emit_inst_mem_rec(max_pc, 0, 1 << 24);
+  dec_indent();
+  emit_line("))))");
+  dec_indent();
+}
+
+static Data* scm_sr_take_data(Data* data, int n) {
+  int i = 0;
+  for (; i < n && data; i++) {
+    data = data->next;
+  }
+  return data;
+}
+
+static void scm_sr_emit_data_mem_rec(Data* data, int from, int to) {
+  if (data == NULL) {
+    emit_line("()");
+  } else if (from + 1 >= to) {
+    emit_line("(%s)", scm_sr_imm_str(data->v));
+  } else {
+    int mid = (from + to)/2;
+    emit_line("(");
+    inc_indent();
+    scm_sr_emit_data_mem_rec(data, from, mid);
+    emit_line(".");
+    scm_sr_emit_data_mem_rec(scm_sr_take_data(data, mid - from), mid, to);
+    dec_indent();
+    emit_line(")");
+  }
 }
 
 static void scm_sr_emit_data_mem(Data* data) {
-  (void)data;
-  /* fixme */
+  emit_line("(define-syntax dmem-init!");
+  inc_indent();
+  emit_line("(syntax-rules (quote) ((_ s) (ck s '");
+  inc_indent();
+  scm_sr_emit_data_mem_rec(data, 0, 1 << 24);
+  dec_indent();
+  emit_line("))))");
+  dec_indent();
 }
 
 static const char* SCM_SR_REG_NAMES[6] = {
@@ -195,5 +258,5 @@ void target_scm_sr(Module* module) {
   scm_sr_emit_data_mem(module->data);
   emit_line("");
 
-  scm_sr_emit_file_prologue();
+  scm_sr_emit_file_epilogue();
 }
